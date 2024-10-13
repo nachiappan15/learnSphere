fd55d7adb7b8a0faa684bb627b0e4969d1260102
priyadharshan.r <101190088+DharshanPd11@users.noreply.github.com>
2024-10-13 13:04:07 +0530

    inital ios setup

---
+++ ios/LearnSpere/LearnSpere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnSpere
+//
+//  Created by Priyadharshan Raja on 13/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/LearnSpere/LearnSpereTests/LearnSpereTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSpereTests.swift
+//  LearnSpereTests
+//
+//  Created by Priyadharshan Raja on 13/10/24.
+//
+
+import Testing
+@testable import LearnSpere
+
+struct LearnSpereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
